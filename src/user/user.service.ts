22543f994f0d1cdca6c81e97814f67151e41def6
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-06-03 11:01:45 +0700

    feat: Add Hash Password on Create User

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,13 +3,17 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserEntity } from './entities/user.entity';
 import { PrismaService } from '../prisma/prisma/prisma.service';
+import { hash } from 'argon2';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
-    return this.prismaService.user.create({ data: createUserDto });
+    const passwordHash = await hash(createUserDto.password);
+    return this.prismaService.user.create({
+      data: { ...createUserDto, password: passwordHash },
+    });
   }
 
   async findAll(): Promise<UserEntity[] | []> {
@@ -24,13 +28,22 @@ export class UserService {
     id: number,
     updateUserDto: UpdateUserDto,
   ): Promise<UserEntity | null> {
+    const user = await this.prismaService.user.findUnique({ where: { id } });
+    if (!user) return null;
+
+    const updateData: UpdateUserDto = {};
+    if (updateUserDto.name) updateData.name = updateUserDto.name;
+    if (updateUserDto.email) updateData.email = updateUserDto.email;
+    if (updateUserDto.password)
+      updateData.password = await hash(updateUserDto.password);
+
     return this.prismaService.user.update({
       where: { id: id },
-      data: updateUserDto,
+      data: updateData,
     });
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<UserEntity | null> {
     return this.prismaService.user.delete({ where: { id: id } });
   }
 }
